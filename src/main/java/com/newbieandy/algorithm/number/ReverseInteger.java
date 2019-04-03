17e8695a6330e753656e05ba0bb606e44ebdfe8f
machao <chao.ma@mtime.com>
2019-04-03 18:21:47 +0800

    add linked leetcode

---
+++ src/main/java/com/newbieandy/algorithm/number/ReverseInteger.java
@@ -0,0 +1,29 @@
+package com.newbieandy.algorithm.number;
+
+/**
+ * Example 1:
+ * <p>
+ * Input: 123
+ * Output: 321
+ * Example 2:
+ * <p>
+ * Input: -123
+ * Output: -321
+ * Example 3:
+ * <p>
+ * Input: 120
+ * Output: 21
+ *
+ * @author chao.ma
+ * @Date 2019/4/3 18:12
+ * @Version 1.0
+ */
+public class ReverseInteger {
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+        }
+        return x;
+    }
+}
